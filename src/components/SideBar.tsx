b7c004441a2f27b8e76cb4f21835a94cc6c12f1b
saleszera <crowzcoleslaw@gmail.com>
2021-03-16 20:52:51 -0300

    refatorando App e criando os components de sidebar e content

--- src/components/SideBar.tsx
+++ src/components/SideBar.tsx
@@ -1,3 +1,18 @@
-export function SideBar() {
-  // Complete aqui
+import '../styles/sidebar.scss';
+
+interface SideBarProps{
+  children: React.ReactNode;
+}
+
+export function SideBar({children}: SideBarProps) {
+  return (
+    <nav className="sidebar">
+      <span>Watch<p>Me</p></span>
+
+      <div className="buttons-container">
+        {children}
+      </div>
+  
+    </nav>
+  )
 }
